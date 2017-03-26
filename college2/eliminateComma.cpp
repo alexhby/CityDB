f171b6c821477ddf3823dbf813e07d3cc38e0c4c
YizhouLuo <986732031@qq.com>
2017-03-26 14:52:45 -0400

    college clean comma

--- college2/eliminateComma.cpp
+++ college2/eliminateComma.cpp
@@ -5,40 +5,23 @@ using namespace std;
 
 int main(){
 	ofstream myfile;
-	myfile.open("resultQuery.txt");
+	myfile.open("cleanCollege.csv");
 
-	ifstream input("company.txt");
-	string line1 = "insert into company(pastRank, Name, City, State, zipCode, altitude, latitude) values ";
-	myfile << line1 << "\n";
+	ifstream input("Colleges_and_Universities.csv");
 	string line;
 
 	while(getline(input, line)){
-		istringstream iStr(line);
-		string rank;
-		getline(iStr, rank, '\t');
-		string pastRank;
-		getline(iStr, pastRank, '\t');
-		string dum;
-		getline(iStr, dum, '\t');
-		string tName;
-		getline(iStr, Name, '\t');
-		string Name = "";
-		for(int i=0; i<tName.length(); ++i){
-			if(tName[i] != '\'') Name = Name + tName[i];
+		bool inQuote = false;
+		for(int i=0; i<line.length(); ++i){
+			if(line[i] == ','){
+				if(inQuote) line[i] = ' ';
+			}
+			else if(line[i] == '"'){
+				if(inQuote) inQuote = false;
+				else inQuote = true;
+			}
 		}
-
-		string City;
-		getline(iStr, City, '\t');
-		string State;
-		getline(iStr, State, '\t');
-		string zipCode;
-		getline(iStr, zipCode, '\t');
-		string altitude;
-		getline(iStr, altitude, '\t');
-		string latitude;
-		getline(iStr, latitude, '\t');
-		string temp = "('"+pastRank+"','"+Name+"','"+City+"','"+State+"','"+zipCode+"','"+altitude+"','"+latitude+"'),";
-		myfile << temp << "\n";
+		myfile << line << "\n";
 	}
 	return 0;
 }
