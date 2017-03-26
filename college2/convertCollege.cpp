8ccb359a941c824dbd2b446ef7a9deea2398e0f5
YizhouLuo <986732031@qq.com>
2017-03-26 15:55:53 -0400

    college2 completed

--- college2/convertCollege.cpp
+++ college2/convertCollege.cpp
@@ -3,42 +3,68 @@
 #include <sstream>
 using namespace std;
 
+//FID,IPEDSID,NAME,ADDRESS,ZIP,TELEPHONE,LATITUDE,LONGITUDE,TOT_ENROLL,HOUSING,DORM_CAP
+
 int main(){
 	ofstream myfile;
-	myfile.open("resultQuery.txt");
+	myfile.open("collegeResult.sql");
 
-	ifstream input("company.txt");
-	string line1 = "insert into company(pastRank, Name, City, State, zipCode, altitude, latitude) values ";
+	ifstream input("college2.txt");
+	string line1 = "insert into college2(IPEDSID,NAME,ADDRESS,ZIP,TELEPHONE,LATITUDE,LONGITUDE,TOT_ENROLL,HOUSING,DORM_CAP) values ";
 	myfile << line1 << "\n";
 	string line;
 
 	while(getline(input, line)){
 		istringstream iStr(line);
-		string rank;
-		getline(iStr, rank, '\t');
-		string pastRank;
-		getline(iStr, pastRank, '\t');
-		string dum;
-		getline(iStr, dum, '\t');
-		string tName;
-		getline(iStr, Name, '\t');
-		string Name = "";
-		for(int i=0; i<tName.length(); ++i){
-			if(tName[i] != '\'') Name = Name + tName[i];
+		string FID;
+		getline(iStr, FID, ',');
+		string IPEDSID;
+		getline(iStr, IPEDSID, ',');
+
+		string NAME = "";
+		string tempName;
+		getline(iStr, tempName, ',');
+		for(int i=0; i<tempName.length(); ++i){
+			if(tempName[i] == '\''){
+				NAME += '\\';
+				NAME += '\'';
+				//NAME += " ";
+				//cout << "detected!!!!!!!!!!!" << endl;
+			}
+			else NAME += tempName[i];
 		}
 
-		string City;
-		getline(iStr, City, '\t');
-		string State;
-		getline(iStr, State, '\t');
-		string zipCode;
-		getline(iStr, zipCode, '\t');
-		string altitude;
-		getline(iStr, altitude, '\t');
-		string latitude;
-		getline(iStr, latitude, '\t');
-		string temp = "('"+pastRank+"','"+Name+"','"+City+"','"+State+"','"+zipCode+"','"+altitude+"','"+latitude+"'),";
+		string ADDRESS = "";
+		string tempAdd;
+		getline(iStr, tempAdd, ',');
+		for(int i=0; i<tempAdd.length(); ++i){
+			if(tempAdd[i] == '\''){
+				ADDRESS += '\\';
+				ADDRESS += '\'';
+			}
+			else{
+				ADDRESS += tempAdd[i];
+			}
+		}
+		string ZIP;
+		getline(iStr, ZIP, ',');
+		string TELEPHONE;
+		getline(iStr, TELEPHONE, ',');
+		string LATITUDE;
+		getline(iStr, LATITUDE, ',');
+		string LONGITUDE;
+		getline(iStr, LONGITUDE, ',');
+		string TOT_ENROLL;
+		getline(iStr, TOT_ENROLL, ',');
+		string HOUSING;
+		getline(iStr, HOUSING, ',');
+		string DORM_CAP;
+		getline(iStr, DORM_CAP, ',');
+
+		string temp = "('"+IPEDSID+"','"+NAME+"','"+ADDRESS+"','"+ZIP+"','"+TELEPHONE+"','"+LATITUDE+"','"+LONGITUDE+"','"+TOT_ENROLL+"','"+HOUSING+"','"+DORM_CAP+"'),";
 		myfile << temp << "\n";
 	}
 	return 0;
 }
+
+
