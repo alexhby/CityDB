97f399b9d03784928f0ecea6c01bcaf25d591bba
cis550 <wuyinjun@seas.upenn.edu>
2017-04-02 14:46:46 -0400

    1st combination

---
+++ db/company/convertSQL.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(){
+	ofstream myfile;
+	myfile.open("resultQuery.txt");
+
+	ifstream input("company.txt");
+	string line1 = "insert into company(pastRank, Name, City, State, zipCode, latitude, longitude) values ";
+	myfile << line1 << "\n";
+	string line;
+
+	while(getline(input, line)){
+		istringstream iStr(line);
+		string rank;
+		getline(iStr, rank, '\t');
+		string pastRank;
+		getline(iStr, pastRank, '\t');
+		string dum;
+		getline(iStr, dum, '\t');
+		string tName;
+		getline(iStr, Name, '\t');
+		string Name = "";
+		for(int i=0; i<tName.length(); ++i){
+			if(tName[i] != '\'') Name = Name + tName[i];
+		}
+
+		string City;
+		getline(iStr, City, '\t');
+		string State;
+		getline(iStr, State, '\t');
+		string zipCode;
+		getline(iStr, zipCode, '\t');
+		string latitude;
+		getline(iStr, latitude, '\t');
+		string longitude;
+		getline(iStr, longitude, '\t');
+		string temp = "('"+pastRank+"','"+Name+"','"+City+"','"+State+"','"+zipCode+"','"+latitude+"','"+longitude+"'),";
+		myfile << temp << "\n";
+	}
+	return 0;
+}
